5ebefe26a2ef10c080c7ca943aafbbc0d177eacf
jetski.ci <gidrokisik@gmail.com>
2024-04-07 14:11:27 +0500

    Adding a form class and the ability to add new cases

--- src/components/Item.ts
+++ src/components/Item.ts
@@ -1,16 +1,16 @@
+import { IItem } from '../types'
+
 export class Item {
-  protected itemElement: HTMLElement
-  protected title: HTMLElement
+	protected itemElement: HTMLElement
+	protected title: HTMLElement
 
-  constructor(template: HTMLTemplateElement) {
-    this.itemElement = template.content
-      .querySelector('.todo-item')
-      .cloneNode(true) as HTMLElement
-    this.title = this.itemElement.querySelector('.todo-item__text')
-  }
+	constructor(template: HTMLTemplateElement) {
+		this.itemElement = template.content.querySelector('.todo-item').cloneNode(true) as HTMLElement
+		this.title = this.itemElement.querySelector('.todo-item__text')
+	}
 
-  render(item: string) {
-    this.title.textContent = item
-    return this.itemElement
-  }
+	render(item: IItem) {
+		this.title.textContent = item.name
+		return this.itemElement
+	}
 }
