e84bd1c55d1366a6b1f0f0e07b11188412c0da24
jetski.ci <gidrokisik@gmail.com>
2024-04-07 12:06:07 +0500

    create item component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import './styles/styles.css'
+import { createItem } from './components/item'
 import { todos } from './utils/constants'
 
 const contentElement = document.querySelector('.todos__list')
@@ -7,16 +8,7 @@ const template = document.querySelector(
   '#todo-item-template'
 ) as HTMLTemplateElement
 
-function createItem(name: string) {
-  const itemElement = template.content
-    .querySelector('.todo-item')
-    .cloneNode(true) as HTMLElement
-  const title = itemElement.querySelector('.todo-item__text')
-  title.textContent = name
-  return itemElement
-}
-
 todos.forEach((item) => {
-  const itemElement = createItem(item)
+  const itemElement = createItem(template, item)
   contentElement.prepend(itemElement)
 })
