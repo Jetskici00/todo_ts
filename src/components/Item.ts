ad34fa0a06c0667b71a3ffb2249468f35de81d12
jetski.ci <gidrokisik@gmail.com>
2024-04-08 21:16:27 +0500

    the presenter, the functionality of creating a copy and deleting a case

--- src/components/Item.ts
+++ src/components/Item.ts
@@ -1,13 +1,31 @@
 import { IItem } from '../types'
 
-export class Item {
+export interface IViewItem {
+	id: string
+	name: string
+	render(item: IItem): HTMLElement
+	setDeleteHandler(handleDeleteItem: Function): void
+	setCopyHandler(handleCopyItem: Function): void
+}
+
+export interface IViewItemConstructor {
+	new (template: HTMLTemplateElement): IViewItem
+}
+
+export class Item implements IViewItem {
 	protected itemElement: HTMLElement
 	protected title: HTMLElement
+	protected deleteButton: HTMLButtonElement
+	protected copyButton: HTMLButtonElement
 	protected _id: string
+	protected handleDeleteItem: Function
+	protected handleCopyItem: Function
 
 	constructor(template: HTMLTemplateElement) {
 		this.itemElement = template.content.querySelector('.todo-item').cloneNode(true) as HTMLElement
 		this.title = this.itemElement.querySelector('.todo-item__text')
+		this.deleteButton = this.itemElement.querySelector('.todo-item__del')
+		this.copyButton = this.itemElement.querySelector('.todo-item__copy')
 	}
 
 	set id(value: string) {
@@ -24,6 +42,20 @@ export class Item {
 
 	get name(): string {
 		return this.title.textContent || ''
+	}
+
+	setDeleteHandler(handleDeleteItem: Function) {
+		this.handleDeleteItem = handleDeleteItem
+		this.deleteButton.addEventListener('click', (evt) => {
+			this.handleDeleteItem(this)
+		})
+	}
+
+	setCopyHandler(handleCopyItem: Function) {
+		this.handleCopyItem = handleCopyItem
+		this.copyButton.addEventListener('click', (evt) => {
+			this.handleCopyItem(this)
+		})
 	}
 
 	render(item: IItem) {
